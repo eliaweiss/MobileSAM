8f93194fe51c420bd8876dce4bb61d2116248785
Elia <elia.weiss@gmail.com>
2024-04-10 15:10:47 +0545

    utils

---
+++ notebooks/Utils.py
@@ -0,0 +1,68 @@
+
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import cv2
+
+
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+
+#########################################
+def plot_rotatedResult(img, rotated_cells):
+    tmpImg = np.array(img)
+    for cell in rotated_cells:
+        rotated_bbox = np.array(cell['bbox'])
+        color = np.int0(np.random.random(3)*255).tolist()
+        cv2.drawContours(tmpImg, [rotated_bbox], 0, color, 2)  
+    pil_img = Image.fromarray(tmpImg)
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    plt.axis('off')
+    plt.show() 
+  
+#########################################
+def plot_results(model, pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                fill=False, color=c, linewidth=3))
+        cl = p.argmax().item()
+        score = p[cl]
+        text = f'{model.config.id2label[cl]}: {score:0.2f}'
+        
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+        
+#########################################
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    img = np.ones((anns.shape[1], anns.shape[2], 4))
+    # img[:,:,3] = 0
+    for ann in range(anns.shape[0]):
+        m = anns[ann].bool()
+        m=m.cpu().numpy()
+        color_mask = np.concatenate([np.random.random(3), [1]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+
+#########################################
+def plot_annotations(image, anns):
+    plt.figure(figsize=(10,10))
+    background=np.ones_like(image)*255
+    plt.imshow(background)
+    show_anns(anns)
+    plt.axis('off')
+    plt.show() 
+    # plt.savefig("{}".format("./out/result.jpg"), bbox_inches='tight', pad_inches = 0.0)         
