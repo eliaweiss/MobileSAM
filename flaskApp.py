99c504510441547e8e6568fde2905addba8e1227
Elia <elia.weiss@gmail.com>
2024-04-17 11:05:14 +0545

    FlaskUtil

--- flaskApp.py
+++ flaskApp.py
@@ -52,17 +52,6 @@ CORS(app)
 def home():
     return "Hello, World!"
 
-def base64_to_cv2(base64_string):
-    # Decode base64 string to bytes
-    image_bytes = base64.b64decode(base64_string)
-    
-    # Convert bytes to numpy array
-    nparr = np.frombuffer(image_bytes, np.uint8)
-    
-    # Decode numpy array to image
-    image_cv2 = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    return image_cv2
 
 @app.route("/detectTbl", methods=["POST", "GET", "OPTIONS"])
 def detectTbl():  
@@ -71,11 +60,7 @@ def detectTbl():
     
     # Accessing the value of the 'tst' key in the JSON data
     base64_string = request_data.get('image')
-    img = base64_to_cv2(base64_string)
-
-    
-    # cv2.imwrite("img.jpg",img)
-    img_pil = Image.fromarray(img)
+    img_pil = base64_to_pil(base64_string)
     origSize = img_pil.size
     # Resize the image
     img_pil.thumbnail((1000, 1000)) 
@@ -87,22 +72,15 @@ def detectTbl():
     }
     for score,bb in list(zip(probas, boxes)):
         bb = np.intp(bb).tolist()
-        # cv2.rectangle(img,(bb[0],bb[1]),(bb[2],bb[3]),(0,0,255),5)
         res['detectTbl'].append({
             "score": round(score, 2),
             "bbox": bb
         })
-        
-    # cv2.imwrite("img.jpg",img)
-        
-      
-    
-    # Do something with the data
-    # print("Received data:", request_data)
-    # print("Value of 'tst':", tst_value)
     
     # Return a response if needed
     return json.dumps(res)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flaskUtil/FlaskUtil.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+import base64
+from PIL import ExifTags, Image
+
+
+class FlaskUtil:
+     
+    ################################
+    def base64_to_cv2(base64_string):
+        # Decode base64 string to bytes
+        image_bytes = base64.b64decode(base64_string)
+        
+        # Convert bytes to numpy array
+        nparr = np.frombuffer(image_bytes, np.uint8)
+        
+        # Decode numpy array to image
+        image_cv2 = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
+        return image_cv2
+     
+    ################################
+    def base64_to_pil(base64_string):
+        img = FlaskUtil.base64_to_cv2(base64_string)
+        # cv2.imwrite("img.jpg",img)
+        img_pil = Image.fromarray(img)
+        return img_pil     
+     
