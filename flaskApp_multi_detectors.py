19bcff983fda3a621cfdf3555746bdda06bc81fe
Elia <elia.weiss@gmail.com>
2024-04-23 10:34:20 +0545

    flask

--- flaskApp_multi_detectors.py
+++ flaskApp_multi_detectors.py
@@ -43,14 +43,51 @@ tblDec = TableDetect()
 tblStructDetect = TblStructureDetect()       
 from ultralyticsplus import YOLO, render_result
 
+# load model
+model_yolov8 = YOLO('keremberke/yolov8m-table-extraction')
+
+# set model parameters
+model_yolov8.overrides['conf'] = 0.25  # NMS confidence threshold
+model_yolov8.overrides['iou'] = 0.45  # NMS IoU threshold
+model_yolov8.overrides['agnostic_nms'] = False  # NMS class-agnostic
+model_yolov8.overrides['max_det'] = 1000  # maximum number of detections per image
+
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+
+
+# you can specify the revision tag if you don't want the timm dependency
+processor_detr = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50") #, revision="no_timm")
+model_detr = DetrForObjectDetection.from_pretrained("TahaDouaji/detr-doc-table-detection") #, revision="no_timm")
+
 @app.route('/')
 def home():
     return "Hello, World!"
 
+def model_yolov8_detect(image):
+    boxes = []
+    labels = []
+    scores = []
+    results = model_yolov8.predict(image)
+    
+    for res in results:
+        boxes.extend(np.intp(res.boxes.xyxy).tolist())
+        labels.extend(res.boxes.cls.tolist())
+        scores.extend(res.boxes.conf.tolist())
+    return scores, boxes, labels
 
-################################################################
-################################################################
+def tblDec_detr(image, threshold = 0.65):
+    inputs = processor_detr(images=image, return_tensors="pt")
+    outputs = model_detr(**inputs)
 
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor_detr.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=threshold)[0]
+    return results["scores"].tolist(), np.intp(results["boxes"].tolist()).tolist()
 
 @app.route("/detectTbl", methods=["POST", "GET", "OPTIONS"])
 def detectTbl():  
@@ -62,68 +99,24 @@ def detectTbl():
     # Accessing the value of the 'tst' key in the JSON data
     base64_string = request_data.get('image')
     img = FlaskUtil.base64_to_cv2Img(base64_string)
-    origSize = Image.fromarray(img).size
-    # img = LineCvUtils.resize_keep_ratio(img, IMAGE_SIZE)
-    # img_pil = Image.fromarray(img)
     img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     
-    print("rescale",img_pil.size, "origSize",origSize)   
-    probas, boxes = tblDec.detectTables(img_pil) # , origSize=origSize
-    # for bb in boxes:
-    #     l,b,r,t = bb
-    #     cv2.rectangle(img, (l,b),(r,t),(0,0,255),5)
-    #     cv2.imwrite("img_tblDet.jpg", img)
-
-    
-    extractTblStructure = request_data.get('extractTblStructure')
-    if request_data.get('extractCtr') or extractTblStructure:
-        ctrList = []
-        tableCells = []
-        anns = sam.process(img_pil,boxes)
-        for cc, (ann, box, prob) in enumerate(zip(anns, boxes,probas)):
-            alignTable_processor = AlignTable_Processor(img_pil, annotation=ann, tblBox=box)
-            ctr = alignTable_processor.getTblApproxCtr()
-            # cv2.drawContours(img, [ctr], 0, (0, 255, 0), 4) 
-            # cv2.imwrite("img_ctr.jpg",img)            
-            ctrList.append(ctr)
-            boxes[cc] = alignTable_processor.getCropBBox()
-            if extractTblStructure:
-                tbl_patch_pil = alignTable_processor.getAlignTable()
-                cells = tblStructDetect.detectTableStructure(tbl_patch_pil)
-                rotated_cells =  alignTable_processor.unRotateAllCell(cells)  
-                
-                FlaskUtil.resizeRotatedCells(rotated_cells, img_pil.size, origSize)
-                                        
-                # applyRotatedResult(img, rotated_cells)
-                # cv2.imwrite("img_cell.jpg", img)                 
-                tableCells.append(rotated_cells)         
-    else:
-        ctrList = FlaskUtil.boxesToCtrs(boxes)
-    if not extractTblStructure:
-        tableCells = np.empty((len(boxes), 0), dtype=int).tolist()
+    # probas, boxes,_ = model_yolov8_detect(img) # , origSize=origSize
+    probas, boxes = tblDec_detr(img_pil) # , origSize=origSize
             
     res = {
         "detectTbl": [],
     }        
     
-    for score,bb,ctr,cells in list(zip(probas, boxes, ctrList,tableCells)):
-        ctr = FlaskUtil.resizePoints(ctr, img_pil.size, origSize)
-        ctr = np.intp(ctr).tolist()
-        bb = FlaskUtil.resizeBox(bb, img_pil.size, origSize)
+    for score,bb in list(zip(probas, boxes)):
         res['detectTbl'].append({
             "score": round(score, 2),
             "bbox": bb,
-            "ctr": ctr,
-            "cells": cells,
         })
     
     print("------ detectTbl time: (s): %s" % round(time.time() - start, 2))
     
     return json.dumps(res)
-
-
-        # cv2.drawContours(img, [ctr], 0, (0, 255, 0), 4)  # Green bounding box with thickness 2
-        # cv2.imwrite("img_ctr.jpg",img)
 
 
 ################################################################
